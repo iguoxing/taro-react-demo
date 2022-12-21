5061843eb5b59c7af1a4ce27cae34e99db1e87dd
ardenzhao <iguoxing@163.com>
2022-12-21 11:32:45 +0800

    分段器

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
 // eslint-disable-next-line import/first
-import { AtButton } from 'taro-ui'
+import { AtButton, AtSegmentedControl } from 'taro-ui'
 
 export default class Index extends Component {
 
@@ -16,11 +16,31 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
+  // eslint-disable-next-line react/sort-comp
+  constructor (){
+    super(...arguments)
+    this.state = {
+      current: 0
+    }
+  }
+
+  handleClick (value) {
+    console.log(value)
+    this.setState({
+      current: value
+    })
+  }
+
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>按钮文案</AtButton>
+        <AtSegmentedControl
+          values={['关注页', '发现页']}
+          onClick={this.handleClick.bind(this)}
+          current={this.state.current}
+        />
+        {this.state.current===0?<view>关注页内容</view>:null}
+        {this.state.current===1?<view>发现页内容</view>:null}
       </View>
     )
   }
